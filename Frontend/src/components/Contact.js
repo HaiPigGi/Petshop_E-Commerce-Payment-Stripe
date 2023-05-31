5ce655318ca276500ad03f816b0746e30e046cd9
HaiPigGi <119752348+HaiPigGi@users.noreply.github.com>
2023-05-31 11:32:38 +0700

    Update code

--- Frontend/src/components/Contact.js
+++ Frontend/src/components/Contact.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
 import ContactPage from './pages/ContactPage';
+import NavbarUser from './pages/NavbarUser';
+import NavbarComp from './pages/NavbarComp';
 export default class Contact extends Component {
   render() {
+    const token = sessionStorage.getItem('token');
+    const email = sessionStorage.getItem('email');
+
       return (
-        <div>
+        <div className='mt-6'>
+        {token && email ? <NavbarUser /> : <NavbarComp />}
+          <br/>
         <ContactPage/>
       </div>
       )
--- README.md
+++
@@ -1,2 +0,0 @@
-# RPL_Petshop_be_fe
-Ini adalah project rpl dengan menggunakan react js disisi frontend dan node js express di sisi backend
